3106efff562fabc888d8a0de007daf4e96ee95c7
Sactio Swastioyono <email.tyo@gmail.com>
2021-09-09 00:03:28 +0700

    feat: recreate preview env

--- main.go
+++ main.go
@@ -9,11 +9,11 @@ import (
 )
 
 func main() {
-	fmt.Println("This is okteto tests")
+	fmt.Println("This is okteto tests 1233456")
 	r := chi.NewRouter()
 	r.Use(middleware.Logger)
 	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte("halo dunia fana"))
+		w.Write([]byte("halo dunia fana masa sih boong aja lalala llilili"))
 	})
 
 	r.Get("/v1", func(w http.ResponseWriter, r *http.Request) {
